f434755871d7923793b374275fad75d64bffa4a8
Sergej Poltorak <sergej.poltorak@gmail.com>
2020-01-25 00:29:37 +0200

    Initial project commit

---
+++ WeatherCollector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeatherCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

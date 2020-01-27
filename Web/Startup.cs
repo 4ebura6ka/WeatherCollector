6c6b4f81c74bbaaf7faa4b02f6985470542db4cd
Sergej Poltorak <sergej.poltorak@gmail.com>
2020-01-27 22:19:40 +0200

    Reconfigured Web project and WeatherCollector

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure;
 
 namespace Web
 {
@@ -24,6 +26,11 @@ namespace Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContextPool<WeatheCollectorDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("WeatherCollectorDb"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

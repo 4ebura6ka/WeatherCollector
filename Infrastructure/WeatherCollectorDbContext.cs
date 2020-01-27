2771e6939f6db12d0b850b2d4ebb695d32755f6b
Sergej Poltorak <sergej.poltorak@gmail.com>
2020-01-28 01:12:17 +0200

    Added EntityFrameworkCore libraries for migration

---
+++ Infrastructure/WeatherCollectorDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WeatherCollector.Core;
+
+namespace Infrastructure
+{
+    public class WeatherCollectorDbContext : DbContext
+    {
+        public DbSet<CityWeather> cityWeathers { get; set; }
+
+        public WeatherCollectorDbContext(DbContextOptions<WeatherCollectorDbContext> options) : base (options)
+        {
+        }
+    }
+}

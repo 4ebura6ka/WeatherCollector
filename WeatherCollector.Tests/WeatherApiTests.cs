dc24617db0abd744f04882acfb78ce26b51a4e19
Sergej Poltorak <sergej.poltorak@gmail.com>
2020-01-29 07:06:33 +0200

    Added symbolic Test project.

---
+++ WeatherCollector.Tests/WeatherApiTests.cs
@@ -0,0 +1,35 @@
+using IO.Swagger.Api;
+using IO.Swagger.Client;
+using NUnit.Framework;
+using System;
+
+namespace WeatherCollector.Tests
+{
+    public class WeatherApiTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetCityWeatherExceptionTest()
+        {
+            WeatherApi weatherApi = new WeatherApi("http://metasite-weather-api.herokuapp.com");
+            ApiException apiException = new ApiException();
+
+            Exception exception = null;
+
+            try
+            {
+                weatherApi.GetCityWeather(null, null);
+            }
+            catch (ApiException ex)
+            {
+                exception = ex;
+            }
+
+            Assert.IsNotNull(exception);
+        }
+    }
+}
